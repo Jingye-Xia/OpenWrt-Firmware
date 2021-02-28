fb1bc321dd66e4e4bb45c27418af102b81bdc61c
JingyeXia <70971515+Jingye-Xia@users.noreply.github.com>
2021-02-28 15:02:27 +0800

    Update leandiy-part1.sh

--- leandiy-part1.sh
+++ leandiy-part1.sh
@@ -18,3 +18,4 @@
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git vssr https://github.com/jerrykuku/luci-app-vssr' feeds.conf.default
+sed -i '$a src-git maxminddb https://github.com/jerrykuku/lua-maxminddb' feeds.conf.default
