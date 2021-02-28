755a01b13db6285df07cc4bcff680066b9448819
JingyeXia <70971515+Jingye-Xia@users.noreply.github.com>
2021-02-28 16:33:58 +0800

    Update lienoldiy-part1.sh

--- lienoldiy-part1.sh
+++ lienoldiy-part1.sh
@@ -17,3 +17,6 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+cd package/lean
+git clone https://github.com/jerrykuku/luci-app-vssr.git
+git clone https://github.com/jerrykuku/lua-maxminddb.git
